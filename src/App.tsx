91ae5027f39c0c1292ef259b0c31eb5a18f4b7c1
Maurice Heumann <mauriceheumann@gmail.com>
2022-12-10 16:20:26 +0100

    Fix resolution for small screens

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
     <div className="App bg-[#151515] text-gray-100">
       <div className="grid-container flex flex-col align-middle items-center justify-center text-center">
         <span className="mt-[-15vh] bg-[#151515] p-5 same-bg-shadow">
-          <h1 className="md:text-5xl font-bold text-3xl">
+          <h1 className="md:text-5xl sm:text-3xl font-bold text-2xl">
             Call of Duty:&nbsp;
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-red-600">
               Black Ops III
             </span>
             <br />
           </h1>
-          <h2 className="md:text-4xl text-2xl font-bold pt-2 md:pt-4">
+          <h2 className="md:text-4xl sm:text-2xl text-xl font-bold pt-2 md:pt-4">
             The way it was meant to be.
           </h2>
           <span className="button-container text-center flex gap-8 mt-8 md:mt-12 align-middle items-center justify-center">

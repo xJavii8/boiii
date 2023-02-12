5c1b898be7ee90a40a20086abd9c94de0b411829
Maurice Heumann <mauriceheumann@gmail.com>
2023-02-12 18:59:01 +0100

    Spacing fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function generateFeatureBox(icon: string, title: string, message: string) {
   return (
     <div className="feature-box" key={title}>
-      <span className="title"><i className={"icon boiii-text-narrow fa-solid fa-" + icon}></i> {title}</span>
+      <span className="title">
+        <i className={"icon boiii-text-narrow fa-solid fa-" + icon}></i> {title}
+      </span>
       <span className="message">{message}</span>
     </div>
   );
@@ -17,9 +19,7 @@ function App() {
         <span className="mt-[-15vh] p-5 same-bg-shadow cursor-default">
           <h1 className="md:text-5xl sm:text-3xl font-bold text-2xl">
             Call of Duty:&nbsp;
-            <span className="boiii-text">
-              Black Ops III
-            </span>
+            <span className="boiii-text">Black Ops III</span>
             <br />
           </h1>
           <h2 className="md:text-4xl sm:text-2xl text-xl font-bold pt-2 md:pt-4 text-[#eeebe79d]">
@@ -48,20 +48,46 @@ function App() {
 
         {/*<div className="mouse-scroll"><span></span></div>*/}
       </div>
-      <div className="separator"><div></div></div>
+      <div className="separator">
+        <div></div>
+      </div>
       <div className="feature-container">
-        <span className="w-100 font-bold md:text-4xl sm:text-2xl text-xl text-center block text-[#f4f0ebc6] cursor-default">What does <span className="boiii-text">
-              BOIII
-            </span> offer?</span>
-        <div className="mb-12 md:mt-8 md:mb-16"></div>
+        <span className="xl:mt-8 w-100 font-bold md:text-4xl sm:text-2xl text-xl text-center block text-[#f4f0ebc6] cursor-default">
+          What does <span className="boiii-text">BOIII</span> offer?
+        </span>
+        <div className="mb-12 md:mb-24 xl:mb-32"></div>
         <div className="feature-grid">
           {[
-            generateFeatureBox("lock", "Improved Security", "Security vulnerabilities have been patched providing a safe gaming experience."),
-            generateFeatureBox("code", "Improved Stability", "Issues and bugs have been fixed resulting in fewer crashes and instabilities."),
-            generateFeatureBox("gauge-high", "Improved Performance", "Performance issues have been addressed leading to higher FPS and smoother gameplay."),
-            generateFeatureBox("trophy", "Ranked Servers", "Unlike in the original game, ranking up on dedicated servers is possible."),
-            generateFeatureBox("cloud", "Workshop Support", "Custom maps and content from the Steam workshop is completely supported."),
-            generateFeatureBox("gamepad", "Controller Support", "Want to play using mouse and keyboard or your favourite gamepad? You decide."),
+            generateFeatureBox(
+              "lock",
+              "Improved Security",
+              "Security vulnerabilities have been patched providing a safe gaming experience."
+            ),
+            generateFeatureBox(
+              "code",
+              "Improved Stability",
+              "Issues and bugs have been fixed resulting in fewer crashes and instabilities."
+            ),
+            generateFeatureBox(
+              "gauge-high",
+              "Improved Performance",
+              "Performance issues have been addressed leading to higher FPS and smoother gameplay."
+            ),
+            generateFeatureBox(
+              "trophy",
+              "Ranked Servers",
+              "Unlike in the original game, ranking up on dedicated servers is possible."
+            ),
+            generateFeatureBox(
+              "cloud",
+              "Workshop Support",
+              "Custom maps and content from the Steam workshop is completely supported."
+            ),
+            generateFeatureBox(
+              "gamepad",
+              "Controller Support",
+              "Want to play using mouse and keyboard or your favourite gamepad? You decide."
+            ),
           ]}
         </div>
       </div>

a41ef91499eb7ae22b60ae6f299636f6afc6ee85
Maurice Heumann <mauriceheumann@gmail.com>
2023-02-12 18:48:20 +0100

    Fix large screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "auto-1": "repeat(1, auto)",
+        "auto-2": "repeat(2, auto)",
+        "auto-3": "repeat(3, auto)",
+      },
+    },
   },
   plugins: [],
 };
